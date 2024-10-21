2b005e3c1d44882105c973a17cd1dc275a054291
karinagassanova <karinegassanova@gmail.com>
2024-10-21 17:17:55 +0100

    Added modal component for view tokens.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
-// @ts-ignore
 import { AppComponent } from './app.component';
-// @ts-ignore
 import { MyInterceptor } from './my-interceptor.service';
-import {DiagramComponent} from "./components/diagram/diagram.component";
-import {FormsModule} from "@angular/forms";
-
-import {CommonModule, JsonPipe, NgClass, NgForOf, NgIf} from '@angular/common';
-import {ButtonsComponent} from "./components/buttons/buttons.component";
-import {SharedService} from "./services/sharedservice";
+import { DiagramComponent } from "./components/diagram/diagram.component";
+import { FormsModule } from "@angular/forms";
+import { SharedService } from "./services/sharedservice";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiagramComponent,
-    ButtonsComponent,
+    DiagramComponent
   ],
   imports: [
-    CommonModule,
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    JsonPipe,
-    NgClass,
-    NgForOf,
-    NgIf,
     NgbModule,
   ],
   providers: [
-    provideHttpClient(
-      withInterceptorsFromDi()
-    ),
+    provideHttpClient(withInterceptorsFromDi()),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: MyInterceptor,
@@ -42,10 +29,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     },
     SharedService
   ],
-  exports: [
-    DiagramComponent
-  ],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
---
+++ src/app/components/modal/modal-component.ts
@@ -0,0 +1,32 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'ngbd-modal-content',
+  standalone: true,
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Token Data</h4>
+      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body">
+      <pre>{{ formattedTokens }}</pre> <!-- Use the formattedTokens property -->
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-secondary" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `,
+})
+export class NgbdModalContent implements OnInit {
+  activeModal = inject(NgbActiveModal);
+  @Input() tokens: Array<{ token: string }> = []; // Accept an array of tokens
+
+  // Computed property to format tokens
+  get formattedTokens(): string {
+    return this.tokens.map(e => `BFID: ${e.token}`).join('\n'); // Format the output
+  }
+
+  ngOnInit() {
+    console.log('Tokens received in modal:', this.tokens); // Log the tokens received
+  }
+}
