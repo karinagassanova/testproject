edb14544d48e8cd9b3630345ba24d3b91969a91b
karinagassanova <karinegassanova@gmail.com>
2024-11-18 14:37:13 +0000

    fixed folding issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -416,7 +416,7 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   iframeOutlineColor: string = 'black';
-
+  isTabsFolded: boolean = false;
   isIframeFolded: boolean = false;
 
   toggleIframeFold() {
